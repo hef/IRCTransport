f20123083f4976e95d4b69a579c21e0ef446ebfd
hef <hef@pbrfrat.com>
2011-03-12 20:25:41 -0600

    attempts reconnection to irc on failure
    Basically just schedules a reconnect task

---
+++ src/main/java/hef/IRCTransport/Connect.java
@@ -0,0 +1,63 @@
+package hef.IRCTransport;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.jibble.pircbot.IrcException;
+import org.jibble.pircbot.NickAlreadyInUseException;
+
+/** Connection Task
+ * This class us a Runnable class for connecting to the irc server.  It will reschedule a connection
+ * @author hef
+ *
+ */
+public class Connect implements Runnable {
+	private IrcAgent agent;
+	private static final Logger log = Logger.getLogger("Minecraft");
+	/** Create a Connection instance
+	 * Pass this from IrcAgent so we have access
+	 * @param agent pass in this from the calling IrcAgent
+	 */
+	public Connect(IrcAgent agent)
+	{
+		this.agent=agent;
+	}
+	/** Run the connection task
+	 * This task will schedule another connection task if it fails to connect.
+	 */
+	public void run()
+	{
+		try {
+			agent.setNick(agent.getPlayer().getName());
+			if(agent.getServer()==null)
+				agent.connect(agent.getPlugin().getIrcServer());
+			else
+				agent.reconnect();
+		} catch (NickAlreadyInUseException e) {
+			//This should not happen.
+			log.log(Level.SEVERE, e.getMessage(), e);
+		} catch (IOException e) {
+			if(e.getMessage().equalsIgnoreCase("Connection refused"))
+			{
+				agent.getPlayer().sendMessage("Failed to connect to Chat Server.");
+				//400 seems to be 20 seconds
+				agent.getPlugin().getServer().getScheduler().scheduleAsyncDelayedTask(agent.getPlugin(), this, 400);
+			}
+			else
+			{
+				log.log(Level.SEVERE, e.getMessage(), e);
+			}
+		} catch (IrcException e) {
+			log.log(Level.SEVERE, e.getMessage(), e);
+		}
+		
+		agent.getPlayer().setDisplayName(agent.getNick());
+		if(!agent.getPlugin().getAutoJoin().equals(""))
+		{
+			agent.setActiveChannel(agent.getPlugin().getAutoJoin());
+			agent.joinChannel(agent.getActiveChannel());
+		}
+	}
+
+}
--- src/main/java/hef/IRCTransport/IrcAgent.java
+++ src/main/java/hef/IRCTransport/IrcAgent.java
@@ -3,14 +3,10 @@
  */
 package hef.IRCTransport;
 
-import java.io.IOException;
-
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.bukkit.entity.Player;
-import org.jibble.pircbot.IrcException;
-import org.jibble.pircbot.NickAlreadyInUseException;
 import org.jibble.pircbot.PircBot;
 import org.jibble.pircbot.User;
 
@@ -19,10 +15,9 @@ import org.jibble.pircbot.User;
  *
  */
 public class IrcAgent extends PircBot {
-	//todo protect player again
-	Player player;
+	private Player player;
 	private String activeChannel;
-	final IRCTransport plugin;
+	final private IRCTransport plugin;
 	private static final Logger log = Logger.getLogger("Minecraft");
 	//flag to indicate we should not reconnect
 	private boolean shutdown; 
@@ -31,23 +26,22 @@ public class IrcAgent extends PircBot {
 	 * 
 	 */
 	public IrcAgent(IRCTransport instance, Player player) {
-		//TODO: see how long this takes to construct
 		this.plugin = instance;
 		this.player = player;
 		this.shutdown = false;
 		setLogin(player.getName());
 		super.setAutoNickChange(true);
-		connect(plugin.getIrcServer(), player.getName());
+		new Connect(this).run();
 		
-		if(!plugin.getAutoJoin().equals(""))
+		if(!getPlugin().getAutoJoin().equals(""))
 		{
-			activeChannel = plugin.getAutoJoin();
+			activeChannel = getPlugin().getAutoJoin();
 			joinChannel(activeChannel);
 		}
 	}
 	public void log(String line)
 	{
-		if(plugin.isVerbose())
+		if(getPlugin().isVerbose())
 		{
 			log.log(Level.INFO,line);
 		}
@@ -62,48 +56,41 @@ public class IrcAgent extends PircBot {
 	{
 		this.activeChannel = channel;
 	}
-	
-	private void connect(String server, String nick)
+	//sets the nick to attempt to use.
+	//
+	/** Set name to attempt to use at login
+	 * This function is not the same as changeNick(String name)
+	 * you probably don't want this function
+	 * @param name the name to attempt to use.
+	 */
+	public void setNick(String name)
 	{
-		//TODO get rid of this line
-		player.sendMessage("attempting to connect to chat server");
-		try{
-			setName(nick);
-			//this doesn't have much in game affect, but might help other plugins.
-			player.setDisplayName(nick);
-			super.connect(server);
-		}  catch (NickAlreadyInUseException e) {
-			  //This should not be called anymore.
-			log.log(Level.SEVERE, e.getMessage(), e);
-		} catch (IOException e) {
-			//TODO schedule a reconnect attempte
-			System.out.println("IOException: Failed to connect to irc server: " + server);
-			log.log(Level.SEVERE, e.getMessage(), e);
-		} catch (IrcException e) {
-			System.out.println("IrcException: Failed to connect to irc server: " + server);
-			log.log(Level.SEVERE, e.getMessage(), e);
-		}
-		
+		super.setName(name);
 	}
+	
 	public void onDisconnect()
 	{
-		player.sendMessage("ChatService Disconnected.");
-		Reconnect reconnectTask = new Reconnect(this);
-		plugin.getServer().getScheduler().scheduleAsyncDelayedTask(this.plugin, reconnectTask);
+		getPlayer().sendMessage("ChatService Disconnected.");
+		if(!shutdown)
+		{
+			//Reconnect reconnectTask = new Reconnect(this);
+			//plugin.getServer().getScheduler().scheduleAsyncDelayedTask(this.plugin, reconnectTask);
+			new Connect(this).run();
+		}
 	}
 	public void onMessage(String channel, String sender, String login, String hostname, String message)
 	{
 		//TODO: replace channel names with numbers
-		player.sendMessage(String.format("[%s] %s: %s", channel, sender, ColorMap.fromIrc(message)));
+		getPlayer().sendMessage(String.format("[%s] %s: %s", channel, sender, ColorMap.fromIrc(message)));
 	}
 	public void onPrivateMessage(String sender, String login, String hostname, String message)
 	{
 		//TODO: check validity of recipient or check for error response
-		player.sendMessage(String.format("%s: %s",sender, message));
+		getPlayer().sendMessage(String.format("%s: %s",sender, message));
 	}
 	public void onAction(String sender, String login, String hostname, String target, String action)
 	{
-		player.sendMessage(String.format("[%s] * %s %s", target, sender, action));
+		getPlayer().sendMessage(String.format("[%s] * %s %s", target, sender, action));
 	}
 	public void sendMessage(String message)
 	{
@@ -111,36 +98,29 @@ public class IrcAgent extends PircBot {
 		sendMessage(activeChannel, message);
 		if(isConnected())
 		{
-			String msg = String.format("[%s] %s: %s", activeChannel, player.getDisplayName(), message);
-			// if verbose, log all chat
-			//if(plugin.isVerbose()) log.log(Level.INFO, msg);
-			player.sendMessage(msg);
+			String msg = String.format("[%s] %s: %s", activeChannel, getPlayer().getDisplayName(), message);
+			getPlayer().sendMessage(msg);
 		}
 	}
 	public void sendAction(String action)
 	{
 		sendAction(activeChannel, action);
-		player.sendMessage(String.format("[%s] * %s %s", activeChannel, player.getDisplayName(), action));
+		getPlayer().sendMessage(String.format("[%s] * %s %s", activeChannel, getPlayer().getDisplayName(), action));
 	}
-	/*public void sendAction(String target, String action)
-	{
-		sendAction(target, action);
-		player.sendMessage(String.format("[%s] %s: %s", target, player.getDisplayName(), action));
-	}*/
 	public void onJoin(String channel, String sender, String login, String hostname) 
 	{
 		//if I joined, change active channel.
 		if(sender.equals(getNick()))
 			activeChannel = channel;
-		player.sendMessage(String.format("[%s] %s has joined.", channel, sender)); //TODO: colorize
+		getPlayer().sendMessage(String.format("[%s] %s has joined.", channel, sender)); 
 	}
 	protected void onNickChange(String oldNick, String login, String hostname, String newNick) 
 	{
-		if(oldNick.equals(player.getDisplayName()))
+		if(oldNick.equals(getPlayer().getDisplayName()))
 		{
-			player.setDisplayName(newNick);
+			getPlayer().setDisplayName(newNick);
 		}
-		player.sendMessage(String.format("%s is now known as %s", oldNick , newNick));
+		getPlayer().sendMessage(String.format("%s is now known as %s", oldNick , newNick));
 	}
 	protected void onUserList(String channel, User[] users)
 	{
@@ -149,7 +129,7 @@ public class IrcAgent extends PircBot {
 		{
 			usersString += user.toString() + " ";
 		}
-		player.sendMessage(String.format("%s members: %s", channel, usersString));
+		getPlayer().sendMessage(String.format("%s members: %s", channel, usersString));
 	}
 	/** Handles response codes not handled by pircbot
 	 * This methods handles irc response codes, slices up the response, and then
@@ -175,7 +155,7 @@ public class IrcAgent extends PircBot {
 	}
 	protected void onErrInviteOnlyChan(String channel, String message)
 	{
-		player.sendMessage(channel + " is invite only");	
+		getPlayer().sendMessage(channel + " is invite only");	
 	}
 	protected void names()
 	{
@@ -188,14 +168,14 @@ public class IrcAgent extends PircBot {
 	
 	@Override
 	protected void onTopic(String channel, String topic, String setBy, long date, boolean changed) {
-		if(plugin.isVerbose())	{
+		if(getPlugin().isVerbose())	{
 			log.log(Level.INFO, String.format("On %tc, %s changed the topic of %s to: %s", date, setBy, channel, topic));
 		}			
 		if(changed){
-			player.sendMessage(String.format("%s changed the topic of %s to: %s", setBy, channel, topic));
+			getPlayer().sendMessage(String.format("%s changed the topic of %s to: %s", setBy, channel, topic));
 		} else {
-			player.sendMessage(String.format("Topic for %s: %s", channel, topic));
-			player.sendMessage(String.format("Topic set by %s [%tc]", setBy, date));
+			getPlayer().sendMessage(String.format("Topic for %s: %s", channel, topic));
+			getPlayer().sendMessage(String.format("Topic set by %s [%tc]", setBy, date));
 		}
 	}
 	protected void topic()
@@ -205,5 +185,11 @@ public class IrcAgent extends PircBot {
 	protected void setTopic(String topic)
 	{
 		setTopic(activeChannel, topic);
+	}
+	public Player getPlayer() {
+		return player;
+	}
+	public IRCTransport getPlugin() {
+		return plugin;
 	}
 }
