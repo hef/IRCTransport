3376f68480bc524fc7167891a5a1cb4d7b292e47
hef <hef@pbrfrat.com>
2011-04-03 16:46:48 -0500

    utojoinkey and yellow system messages

--- src/main/java/hef/IRCTransport/Connect.java
+++ src/main/java/hef/IRCTransport/Connect.java
@@ -59,7 +59,18 @@ public class Connect implements Runnable {
 			agent.getPlayer().setDisplayName(agent.getNick());
 			if(!agent.getPlugin().getAutoJoin().equals(""))
 			{
+				//if no key is set
+				if(!agent.getPlugin().getAutoJoinKey().equals(""))
+				{
+					agent.joinChannel(agent.getPlugin().getAutoJoin());
+				}
+				else
+				{
+					agent.joinChannel(agent.getPlugin().getAutoJoin(),agent.getPlugin().getAutoJoinKey());
+
+				}
 				agent.setActiveChannel(agent.getPlugin().getAutoJoin());
+
 				agent.joinChannel(agent.getActiveChannel());
 			}
 		}
