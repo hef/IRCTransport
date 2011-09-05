9f3439b3adc195cfa1141f10042dc3c022e8256c
hef <hef@pbrfrat.com>
2011-09-04 23:15:43 -0500

    cleaned up AgentSettings checkstyle errors.
    also added package-info, it cleans out more checkstyle errors.

--- src/main/java/hef/IRCTransport/AgentSettings.java
+++ src/main/java/hef/IRCTransport/AgentSettings.java
@@ -9,37 +9,75 @@ import org.bukkit.entity.Player;
 import com.avaje.ebean.validation.NotEmpty;
 import com.avaje.ebean.validation.NotNull;
 
+/** Persistant Agent settings class.
+ * This class utilizes bukkit ebean facilities.
+ * Any Player/agent related information that should persist between sessions
+ * is defined here.
+ */
 @Entity()
 @Table(name = "players")
 public final class AgentSettings {
+    /** The player's chosen name
+     * This is the display name for the player
+     * It is the value we want to retrieve.
+     */
     @NotEmpty
     private String ircNick = null;
 
+    /** The real player name.
+     * This is the real player name not the nick.
+     * This is also the primary key stored in the database.
+     */
     @Id
     @NotNull
     private String playerName;
 
+    /** Mandatory constructor
+     * don't use this constructor, ebean needs it.
+     */
     public AgentSettings() {
     }
 
-    public AgentSettings(Player player) {
+    /** The main constructor.
+     * builds on object based on the players realname.
+     * It will gather the players nick from the database.
+     * @param player the player's real name
+     */
+    public AgentSettings(final Player player) {
         setPlayerName(player.getName());
     }
 
+    /** fetched stored nick
+     * If the player has set the nick in the past, this field will retrieve
+     * it from the database.
+     * @return the nickname to fetch
+     */
     public String getIrcNick() {
         return ircNick;
     }
 
+    /** Fetch the stored player name from the database.
+     * The rest of the data is matched based on this field.
+     * @return the players name
+     */
     public String getPlayerName() {
         return playerName;
     }
 
-    public void setIrcNick(String ircNick) {
-        this.ircNick = ircNick;
+    /** Store off an irc nick.
+     * Sets the nick to store in the database.
+     * @param nick the new nick to use.
+     */
+    public void setIrcNick(final String nick) {
+        this.ircNick = nick;
     }
 
-    // If you don't have this, the persistence stuff gets cranky
-    public void setPlayerName(String playerName) {
-        this.playerName = playerName;
+    /**
+     * If you don't have this, the persistence stuff gets cranky
+     * This isn't used buy the IrcAgent class directly.
+     * @param name The players real login name.
+     */
+    public void setPlayerName(final String name) {
+        this.playerName = name;
     }
 }
---
+++ src/main/java/hef/IRCTransport/package-info.java
@@ -0,0 +1,20 @@
+/** hef's IRC Transport.
+ * IRCTransport is a java plugin for the Bukkit Minecraft Server.
+ *
+ * The plugin acts as a bridge connecting Minecraft's in-game chat to a real
+ * world IRC server.  The plugin creates an IrcAgent on behalf of each player,
+ * and joins a configured irc server/channel.
+ *
+ * Several IRC commands are supported:
+ * /me
+ * /join
+ * /leave
+ * /nick
+ * /names
+ * /topic
+ *
+ * additionally, players can use /channel to change channels.
+ * @author hef
+ * @url https://github.com/hef/IRCTransport
+ */
+package hef.IRCTransport;
